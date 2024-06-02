84437cfdd8d8733afb7ad241b94a8c855d5fa2a7
dansoares8 <danielsoares8296@gmail.com>
2024-06-02 15:01:50 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const openModalButton = documet.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+[openModalButton, closeModalButton, fade].forEach(el) =>{
+    
+}
