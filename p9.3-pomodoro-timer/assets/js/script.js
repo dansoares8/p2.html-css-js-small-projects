65178a67c351fd86f5a1e8c4ef3c0d2037edd9ae
dansoares8 <danielsoares8296@gmail.com>
2024-12-28 15:00:27 -0300

    up

---
+++ p9.3-pomodoro-timer/assets/js/script.js
@@ -0,0 +1,40 @@
+// variaveis
+let workTittle = document.getElementById('work');
+let breakTittle = document.getElementById('break');
+
+let workTime = 25;
+let breakTime = 5;
+
+let seconds = "00";
+
+//display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTittle.classList.add('active');
+}
+
+//start timer
+function start(){
+    //change the time
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
+
+    breakCount = 0;
+
+    //countdown
+    let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        //start
+        seconds = seconds - 1;
+    }
+
+    //start countdown 
+    setInterval(timerFunction, 1000); // 1000 = 1s
+}
